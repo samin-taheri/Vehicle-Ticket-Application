0f2c1bac0ba13a2591a39dfdedb9f46110ab70d3
ozgeAtik <ozgeatik4@gmail.com>
2021-04-05 01:06:45 +0300

    login and signup

---
+++ VehicleTicket/app/src/main/java/com/example/vehicleticket/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.vehicleticket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView textViewUser = (TextView) findViewById(R.id.main_textViewWelcome);
+        textViewUser.setText("Welcome, " + mAuth.getCurrentUser().getEmail());
+
+    }
+
+    public void logout(View view){
+        mAuth.signOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ VehicleTicket/app/src/main/java/com/example/vehicleticket/RegisterActivity.java
@@ -0,0 +1,65 @@
+package com.example.vehicleticket;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void navigateToLogin(View view){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void createUser(View view){
+        EditText editTextEmail = (EditText) findViewById(R.id.register_editTextEmail);
+        String email = editTextEmail.getText().toString();
+
+        EditText editTextPassword = (EditText) findViewById(R.id.register_editTextPassword);
+        String password = editTextPassword.getText().toString();
+
+        EditText editTextConfirmPassword = (EditText) findViewById(R.id.register_editTextConfirmPassword);
+        String confirmPassword = editTextConfirmPassword.getText().toString();
+
+        if (!password.equals(confirmPassword)){
+            Toast.makeText(RegisterActivity.this, "Passwords don't match.", Toast.LENGTH_SHORT).show();
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        System.out.println(user.getEmail());
+                    }else{
+                        Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+
+    }
+
+
+}
